29d90fa83004911f85f710b75ef288071e974fed
egnclvs <40348905+egnclvs@users.noreply.github.com>
2018-06-18 01:50:03 -0300

    Update CalculoBolao.py

--- CalculoBolao.py
+++ CalculoBolao.py
@@ -16,28 +16,15 @@ while True:
     selecao.append(int(input("Quantos gols o %s fez ? " %selecao[1])))
     #regras
     regra1 = selecao[2] == selecao[4] and selecao[3] == selecao[5]
-    
     regra2 = selecao[2] > selecao[4] and selecao[3] > selecao[5]
     regra3 = selecao[2] == selecao[4] and selecao[3] > selecao[5]
     regra4 = selecao[2] > selecao[4] and selecao[3] == selecao[5]
-
     regra5 = selecao[2] < selecao[4] and selecao[3] < selecao[5]
     regra6 = selecao[2] == selecao[4] and selecao[3] < selecao[5]
     regra7 = selecao[2] < selecao[4] and selecao[3] == selecao[5]
-    
     regra8 = selecao[3] > selecao[2] and selecao[5] > selecao[4]
     regra9 = selecao[3] < selecao[2] and selecao[5] < selecao[4]
     regra10 = selecao[2] == selecao[3] and selecao[4] == selecao[5]
-    print(regra1)
-    print(regra2)
-    print(regra3)
-    print(regra4)
-    print(regra5)
-    print(regra6)
-    print(regra7)
-    print(regra8)
-    print(regra9)
-    print(regra10)
     if regra1:
         print("20 pontos")
     elif regra8 and regra5:
